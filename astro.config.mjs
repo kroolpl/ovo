26224ba7c07152e187072869f075fde6a01b1000
qrol <ggglondyn@gmail.com>
2024-11-24 00:53:27 +0100

    Initial commitv13

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
+  adapter: netlify(),
   vite: {
     ssr: {
       noExternal: ['@sanity/image-url']
