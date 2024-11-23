b0801d13dd060e790a49be1c096dff0da3315a62
qrol <ggglondyn@gmail.com>
2024-11-24 00:12:42 +0100

    Initial commitv4

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface Article {
+  _id: string;
+  _type: string;
+  title: string;
+  author: string;
+  tags?: string[];
+  status: 'pending' | 'approved' | 'rejected';
+  rejectionReason?: string;
+  mainImage?: {
+    _type: 'image';
+    asset: {
+      _type: 'reference';
+      _ref: string;
+    };
+  };
+  publishedAt: string;
+  body: {
+    _type: string;
+    children: {
+      _type: string;
+      text: string;
+    }[];
+  }[];
+}
+
+export interface User {
+  app_metadata?: {
+    roles?: string[];
+  };
+} 
