96e93d7ec5f09c941322728294a5cefde1c85ecd
qrol <ggglondyn@gmail.com>
2024-11-24 01:35:35 +0100

    Initial commitv23

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,6 +1,4 @@
 import { createClient } from '@sanity/client'
-import imageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 if (!import.meta.env.PUBLIC_SANITY_PROJECT_ID) {
   throw new Error('Missing PUBLIC_SANITY_PROJECT_ID in environment variables');
@@ -18,11 +16,28 @@ export const client = createClient({
   token: import.meta.env.SANITY_WRITE_TOKEN
 })
 
-const builder = imageUrlBuilder({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-})
+export const urlFor = (source: any) => {
+  if (!source?.asset?._ref) {
+    return '';
+  }
+  
+  const [_file, id, dimension, format] = source.asset._ref.split('-');
+  const [width, height] = dimension.split('x');
 
-export function urlFor(source: SanityImageSource) {
-  return builder.image(source)
-} 
+  return `https://cdn.sanity.io/images/${import.meta.env.PUBLIC_SANITY_PROJECT_ID}/${import.meta.env.PUBLIC_SANITY_DATASET}/${id}-${dimension}.${format}`;
+}
+
+// Helper functions to chain image transformations
+export const imageBuilder = (url: string) => {
+  if (!url) return {
+    width: () => imageBuilder(''),
+    height: () => imageBuilder(''),
+    url: () => ''
+  };
+
+  return {
+    width: (w: number) => imageBuilder(`${url}?w=${w}`),
+    height: (h: number) => imageBuilder(`${url}?h=${h}`),
+    url: () => url
+  };
+}; 
